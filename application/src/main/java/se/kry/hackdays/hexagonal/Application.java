b7e4fdb6bc5cf9623cd72cc0b86e274b69af1fed
Olivier Le Merdy <olivier.lemerdy@kry.se>
2023-01-03 17:10:30 +0100

    Splitting in two modules

--- application/src/main/java/se/kry/hackdays/hexagonal/Application.java
+++ application/src/main/java/se/kry/hackdays/hexagonal/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlatformHackdaysHexagonalApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(PlatformHackdaysHexagonalApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
