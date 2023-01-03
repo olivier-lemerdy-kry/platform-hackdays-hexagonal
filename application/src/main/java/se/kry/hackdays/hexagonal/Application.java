ffb86745dc76ce3716031c2e2b92cfa716f9cbd8
Olivier Le Merdy <olivier.lemerdy@kry.se>
2023-01-03 16:59:52 +0100

    Initial commit

---
+++ application/src/main/java/se/kry/hackdays/hexagonal/Application.java
@@ -0,0 +1,13 @@
+package se.kry.hackdays.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformHackdaysHexagonalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlatformHackdaysHexagonalApplication.class, args);
+  }
+
+}
---
+++ application/src/test/java/se/kry/hackdays/hexagonal/ApplicationTest.java
@@ -0,0 +1,13 @@
+package se.kry.hackdays.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformHackdaysHexagonalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
